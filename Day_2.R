6201ff0f942234c1d9331e800c26bac25a012422
AmierohAbrahams <3559244@myuwc.ac.za>
2021-04-20 20:08:41 +0200

    lecture 2

---
+++ Day_2.R
@@ -0,0 +1,59 @@
+library(tidyverse)
+library(plotly)
+
+set.seed(666)
+r_dat <- data.frame(dat = c(rnorm(n = 1000, mean = 10, sd = 3),
+                            rnorm(n = 1000, mean = 8, sd = 2)),
+                    sample = c(rep("A", 1000), rep("B", 1000)))
+
+# Create histogram
+h <- ggplot(data = r_dat, aes(x = dat, fill = sample)) +
+  geom_histogram(position = "dodge", binwidth = 1, alpha = 0.8) +
+  geom_density(aes(y = 1*..count.., fill = sample), colour = NA, alpha = 0.4) +
+  labs(x = "value")
+h
+
+shapiro.test(r_dat$dat)
+
+r_dat %>% 
+  group_by(sample) %>% 
+  summarise(norm_dat = as.numeric(shapiro.test(dat)[2]))
+
+
+r_dat %>% 
+  group_by(sample) %>% 
+  summarise(sample_var = var(dat))
+
+two_assum <- function(x) {
+  x_var <- var(x)
+  x_norm <- as.numeric(shapiro.test(x)[2])
+  result <- c(x_var, x_norm)
+  return(result)
+}
+
+r_dat %>% 
+  group_by(sample) %>% 
+  summarise(sample_var = two_assum(dat)[1],
+            sample_norm = two_assum(dat)[2])
+
+r_one <- data.frame(dat = rnorm(n = 20, mean = 20, sd = 5),
+                    sample = "A")
+
+t.test(r_one$dat, mu = 20)
+
+r_two <- data.frame(dat = c(rnorm(n = 20, mean = 4, sd = 1),
+                            rnorm(n = 20, mean = 5, sd = 1)),
+                    sample = c(rep("A", 20), rep("B", 20)))
+
+# perform t-test
+# note how we set the `var.equal` argument to TRUE because we know 
+# our data has the same SD (they are simulated as such!)
+t.test(dat ~ sample, data = r_two, var.equal = TRUE)
+
+# Exercise
+ecklonia <- read_csv("data/ecklonia.csv") %>% 
+  gather(key = "variable", value = "value", -species, -site, -ID)
+
+ggplot(data = ecklonia, aes(x = variable, y = value, fill = site)) +
+  geom_boxplot() +
+  coord_flip()
